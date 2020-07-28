037045c05d013f77d71a28af4b9fbb8ab8685767
abdo hassan <abdocoder0@gmail.com>
2020-07-29 00:15:53 +0200

    adding conditions for square contains bombs

--- app.js
+++ app.js
@@ -8,15 +8,36 @@ document.addEventListener('DOMContentLoaded', () => {
     // get shuffled array with random bombs
     const bombsArray = Array(bombAmout).fill('bomb');
     const emptyArray = Array(width * width - bombAmout).fill('valid');
-    console.log('bomb array', bombsArray);
-    console.log('empty array', emptyArray);
     const gameArray = emptyArray.concat(bombsArray);
-    console.log('game array', gameArray);
+    const shuffledArray = gameArray.sort(() => Math.random() - 0.5);
+
     for (let i = 0; i < width * width; i++) {
       const square = document.createElement('div');
       square.setAttribute('id', i);
+      square.classList.add(shuffledArray[i]);
       grid.appendChild(square);
       squares.push(square);
+    }
+
+    // add numbers
+    for (let i = 0; i < squares.length; i++) {
+      let total = 0;
+      const isLeftEdge = i % width === 0;
+      const isRightEdge = i % width === width - 1;
+
+      if (squares[i].classList.contains('valid')) {
+        if (i > 0 && !isLeftEdge && squares[i - 1].classList.contains('bomb'))
+          total++;
+        if (
+          i > 9 &&
+          !isRightEdge &&
+          squares[i + 1 - width].classList.contains('bomb')
+        )
+          total++;
+
+        squares[i].setAttribute('data', total);
+        console.log(squares[i]);
+      }
     }
   }
 
