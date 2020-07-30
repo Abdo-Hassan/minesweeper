cbdca911a15afdafe93896893f60bb888633fa67
abdo hassan <abdocoder0@gmail.com>
2020-07-30 17:24:55 +0200

    adding click on bomb action 💣

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ document.addEventListener('DOMContentLoaded', () => {
       square.classList.add(shuffledArray[i]);
       grid.appendChild(square);
       squares.push(square);
+      // normal click
+      square.addEventListener('click', (e) => {
+        click(square);
+      });
     }
 
     // add numbers
@@ -34,12 +38,39 @@ document.addEventListener('DOMContentLoaded', () => {
           squares[i + 1 - width].classList.contains('bomb')
         )
           total++;
-
+        if (i > 10 && squares[i - width].classList.contains('bomb')) total++;
+        if (
+          i > 11 &&
+          !isLeftEdge &&
+          squares[i - 1 - width].classList.contains('bomb')
+        )
+          total++;
+        if (i < 98 && !isRightEdge && squares[i + 1].classList.contains('bomb'))
+          total++;
+        if (
+          i < 90 &&
+          !isLeftEdge &&
+          squares[i - 1 + width].classList.contains('bomb')
+        )
+          total++;
+        if (
+          i < 88 &&
+          !isRightEdge &&
+          squares[i + 1 + width].classList.contains('bomb')
+        )
+          total++;
+        if (i < 89 && squares[i + width].classList.contains('bomb')) total++;
         squares[i].setAttribute('data', total);
-        console.log(squares[i]);
       }
     }
   }
 
   createBoard();
+
+  // click on square actions
+  function click(square) {
+    if (square.classList.contains('bomb')) {
+      alert('Game Over');
+    }
+  }
 });
